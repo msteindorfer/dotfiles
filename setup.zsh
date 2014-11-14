0d018cf3a592a65c03135df6cad8a7b0fbab7565
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2014-11-14 21:20:29 +0100

    Simplistic setup file that assumes that no files / symlinks are arleady in place.

--- setup.zsh
+++ setup.zsh
@@ -1,1 +1,11 @@
+git submodule init && git submodule update
+
 mkdir ~/.vimbackup
+
+ln -s dotfiles/.gvimrc ~
+ln -s dotfiles/.vim ~
+ln -s dotfiles/.vimrc ~
+
+ln -s dotfiles/.zlogin ~
+ln -s dotfiles/.zsh ~
+ln -s dotfiles/.zshrc ~
