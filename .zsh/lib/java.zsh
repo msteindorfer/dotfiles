d4047cc4904b408d1f41c6920460bfb8942800c5
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2015-12-08 13:37:16 +0100

    Fixing GRAAL_HOME and eclipse alias.

--- .zsh/lib/java.zsh
+++ .zsh/lib/java.zsh
@@ -2,7 +2,7 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home
 export EXTRA_JAVA_HOMES=/Library/Java/JavaVirtualMachines/jdk1.7.0.jdk/Contents/Home
 
 export PATH=$PATH:$HOME/Development/mx
-export GRAAL_HOME=$HOME/Development/jku/basic-graal/jdk1.8.0_60/product
+export GRAAL_HOME=$HOME/Development/jku/mx2/graal/jvmci/jdk1.8.0_60/product
 
-alias eclipse=$HOME/Development/__install/eclipse/eclipse
+alias eclipse=$HOME/Development/__install/eclipse/Eclipse.app/Contents/MacOS/eclipse
 alias eclipse-fmt="eclipse -noSplash -application org.eclipse.jdt.core.JavaCodeFormatter -config <(curl -s https://google-styleguide.googlecode.com/svn/trunk/eclipse-java-google-style.xml)"
