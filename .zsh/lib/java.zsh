dd91e9b756a609abba9c7e42fb44c959b0b4ad8e
Michael Steindorfer <michael.steindorfer@cwi.nl>
2016-01-13 12:00:39 +0100

    Adding config of Java disassembler lib.

--- .zsh/lib/java.zsh
+++ .zsh/lib/java.zsh
@@ -6,3 +6,6 @@ export GRAAL_HOME=$HOME/Development/jku/mx2/graal/jvmci/jdk1.8.0_65/product
 
 alias eclipse=$HOME/Development/__install/eclipse/Eclipse.app/Contents/MacOS/eclipse
 alias eclipse-fmt="eclipse -noSplash -application org.eclipse.jdt.core.JavaCodeFormatter -config <(curl -s https://google-styleguide.googlecode.com/svn/trunk/eclipse-java-google-style.xml)"
+
+# http://nitschinger.at/Printing-JVM-generated-Assembler-on-Mac-OS-X/
+export LD_LIBRARY_PATH=$HOME/lib
